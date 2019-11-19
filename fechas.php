2f4c15670cdd39ccbf27f0d36a3175e5cbbce6a3
Antonio Alarcon <jose.alarcon.mtz@gmail.com>
2019-11-19 03:48:48 +0000

    Para fechas

--- fechas.php
+++ fechas.php
@@ -13,7 +13,16 @@
         
         $fechaConTimestamp = strtotime($ultimaFecha);
         echo $fechaConTimestamp;
-        echo strtotime("now");
+        echo "<br>".strtotime("now");
+        // arreglo con los dias feriados del 2019}
+        $diasFestivos =[
+            '2019' => [],
+            '2020' => [],
+        ];
+        
+        $unDiaMas = $fechaConTimestamp+86400;
+        echo "<br>".$unDiaMas;
+        echo "<br>".strftime("%d-%m-%Y",$unDiaMas);
 
     ?>
 </body>
