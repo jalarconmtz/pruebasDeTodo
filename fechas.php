e9765de15e2a0888f1ef1b0f2469ce25ad7acb09
jalarconmtz <jose.alarcon.mtz@gmail.com>
2019-11-19 19:34:20 -0600

    Prueba de fecha para dimingos

--- fechas.php
+++ fechas.php
@@ -23,6 +23,14 @@
         $unDiaMas = $fechaConTimestamp+86400;
         echo "<br>".$unDiaMas;
         echo "<br>".strftime("%d-%m-%Y",$unDiaMas);
+        echo "<br><br>";
+        $fecha1 = strtotime('2010-10-20'); 
+$fecha2 = strtotime('2010-10-25'); 
+for($fecha1;$fecha1<=$fecha2;$fecha1=strtotime('+1 day ' . date('Y-m-d',$fecha1))){ 
+    if((strcmp(date('D',$fecha1),'Sun')!=0) || (strcmp(date('D',$fecha1),'Sat')!=0)){
+        echo date('Y-m-d D',$fecha1) . '<br />'; 
+    }
+}
 
     ?>
 </body>
